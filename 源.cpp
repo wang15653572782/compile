d5a2e22cfa51132b02d1d22b9d673c1057d955f9
wcf <635678729@qq.com>
2023-03-15 17:59:41 +0800

    编译原理课程设计词法分析功能已实现

--- 源.cpp
+++ 源.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -7,14 +8,10 @@
 #include <cstdio>
 #include <stack>
 #include <fstream>
+#include<iomanip>
 using namespace std;
-typedef pair<string, string> PSS;//存储单个Token 
+typedef pair<int,pair<string, string>> PSS;//存储单个Token 
 vector<PSS> token;
-
-//string operate[] = { "+","-",">","<","=" };
-int T[6][3] = {
-1,3,5,1,1,2,0,0,0,4,3,4,0,0,0,0,0,0
-};
 bool is_num(char ch) {
 	if (ch >= '0' && ch <= '9')return true;
 	return false;
@@ -28,7 +25,13 @@ bool is_other(char ch) {
 	if (is_num(ch) || is_letter(ch))return false;
 	return true;
 }
+void printToken(PSS& token1) {
+	int line = token1.first;
+	auto item = token1.second;
+	//cout << setw(15) <<"行数" << setw(15) << "" << setw(15) << "" << endl;
+	cout <<setw(15)<<line <<setw(15) << item.first <<setw(15)<<item.second<< endl;
 
+}//显示一个单词 
 bool  reservedLookup(string s) {
 	string Keyword[] = { "program","procedure","type","var","then","else","fi","do",
 "endwh","array","of","record","write","return","integer","char","if","EOF","read","while","end","begin"};
@@ -37,66 +40,73 @@ bool  reservedLookup(string s) {
 	}
 	return false;
 };//查找保留字
-
 bool getTokenlist(FILE* fp)//取得当前行所有的token序列
-{
-	int state = 0;//初态
-	//int point = 0;
-	//vector<char> to;
-	
+{	
 	char cp = fgetc(fp);
-	
+	int line = 1;
 	while (cp!=EOF){
 		while (cp == ' ' || cp == '\t' || cp == '\n')
 		{
-
+			if (cp == '\n')line++;
 			cp = fgetc(fp);
 		}//去掉所有的空格，制表符，换行符
 		
-		if (is_letter(cp) || is_num(cp)) {//是字母或是数字
-
-			//int init = p;
-			int state = 0;//初态
-
-			int k = 2;
-			if (is_letter(cp))k = 0;
-			if (is_num(cp))k = 1;
+		if (is_letter(cp)) {//字母
 			string val;
-
-			while (T[state][k] && cp != '.') {
-				state = T[state][k];
+			while (is_letter(cp) || is_num(cp)) {
 				val.push_back(cp);
 				cp = fgetc(fp);
 			}
 			ungetc(cp, fp);
 			PSS ans;
-			if (state == 2) {
-				if (reservedLookup(val)) {
-					ans = { "保留字",val };
-				}
-				else ans = { "标识符",val };
-
-				token.push_back(ans);
+			if (reservedLookup(val)) {
+				ans = { line,{ "保留字",val } };
 			}
+			else {
+				ans = { line,{ "标识符",val } };
+			}
+			
+			//printToken(ans);
+			token.push_back(ans);
+			cp = val.back();
+		}
+
+		if (is_num(cp)) {//数字
+			string val;
+			//val.push_back(cp);
+			while (is_num(cp)) {
+				val.push_back(cp);
+				cp = fgetc(fp);
+				
+			}
+			ungetc(cp, fp);
+			cp = val.back();
+			PSS ans = { line,{ "无符号整数",val } };
+			//printToken(ans);
+			token.push_back(ans);
 		}
 			if (cp == '+' || cp == '-' || cp == '*' || cp == '/' || cp == '(' ||
-				cp == ')' || cp == ';' || cp == '[' || cp == ']' || cp == '=' || cp == '<') {
+				cp == ')' || cp == ';' || cp == '[' || cp == ']' || cp == '=' || cp == '<') {//分界符
 				string tmp;
 				tmp += cp;
-				PSS a_token = { "单分界符",tmp };
+				PSS a_token = { line,{ "单分界符",tmp } };
+				//printToken(a_token);
 				token.push_back(a_token);
 			}
-			if (cp == ':') {//:=
+			if (cp == ':') {//:=双分界符
 				string tmp;
 				tmp += cp;
 				cp = fgetc(fp);
 				if (cp == '=') {
 					tmp += cp;
-					PSS to = { "双分界符",tmp };
+					PSS to = { line,{ "双分界符",tmp } };
+					//printToken(to);
 					token.push_back(to);
 				}
 				else {
 					ungetc(cp, fp);
+					cp = ':';
+					cout << line <<":错误,你是不是忘写了个等号呀，兄弟"<< endl;
 					return false;
 				}
 
@@ -105,50 +115,61 @@ bool getTokenlist(FILE* fp)//取得当前行所有的token序列
 			if (cp == '{') {//注释
 				cp = fgetc(fp);
 				while (cp != '}') {
+					if (cp == '\n')line++;
 					cp = fgetc(fp);
-
+					
 				}
 				if (cp != '}')return false;
-				PSS to = { "注释","{}" };
+				PSS to = { line,{ "注释","{}"} };
+				//printToken(to);
 				token.push_back(to);
 			}
 			if (cp == '.')//数组下标
 			{
 				cp = fgetc(fp);
 				if (cp == '.') {
-					PSS to = { "数组下标",".." };
+					PSS to = { line,{ "数组下标",".." } };
+					//printToken(to);
 					token.push_back(to);
 				}
 				else {
-					PSS to = { "程序结束标志","."};
-					token.push_back(to);
+					PSS to = { line,{ "程序结束标志","."} };
+					if (cp != ' '&&cp!='\n'&&cp!=EOF) {
+						cout << line << ":错误，兄弟，你数组下标是不是忘了一个点呀" << endl;
+						return false;
+					}
+					else {
+						ungetc(cp, fp);
+						cp = '.';
+						//printToken(to);
+						token.push_back(to);
+					}
 				}
 			}
 			if (cp == ',') {//字符标志状态
 				cp = fgetc(fp);
 				if (is_num(cp) || is_letter(cp)) {
 					ungetc(cp, fp);
-					PSS to = { "字符标志状态","," };
-
+					cp = ',';
+					PSS to = { line,{ "字符标志状态","," } };
+					//printToken(to);
+					token.push_back(to);
 				}
 				else {
 					ungetc(cp, fp);
+					cp = ',';
+					cout << line <<":错误，','是字符标志状态，后面应该是字母或数字"<< endl;
 					return false;
 				}
 			}
-			else return false;
+			//else return false;
 			cp = fgetc(fp);
 		}
 	return true;
 }
-void printToken(PSS& token1) {
-	
-	string key = token1.first;
-	string val = token1.second;
-	cout << key << " " << val << endl;
 
-}//显示一个单词 
 void printTokenlist(vector<PSS>& to) {
+	cout << setw(15) << "行数" << setw(15) << "词法信息" << setw(15) << "语义信息" << endl;
 	for (auto a = to.begin(); a != to.end(); a++) {
 		printToken(*a);
 	}
@@ -164,7 +185,7 @@ void ChainToFile(vector<PSS>& Chainhead);//将链表中的Token存入文件
 
 int main() {
 	FILE* fp;
-	if ((fopen_s(&fp,"/input.txt", "r")) == NULL)
+	if ((fp=fopen("input.txt", "r")) == NULL)
 	{
 		printf("文件打开失败\n");
 		exit(1);
@@ -172,15 +193,10 @@ int main() {
 	bool flag = getTokenlist(fp);
 	fclose(fp);
 	if(flag==false){
-		cout << "词法分析失败，程序编写有错误" << endl;
+		cout << "词法分析失败，程序编写有错误，请重新编程" << endl;
 	}
 	else {
 		printTokenlist(token);
 	}
-
-	
-	
-
 	return 0;
-
 }
